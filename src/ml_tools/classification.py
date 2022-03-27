08cf18114c78941bb8386e81d2042fd449011848
jacob <jwgerlach00@gmail.com>
2022-03-27 17:20:54 -0400

    Added docstring

--- src/ml_tools/classification.py
+++ src/ml_tools/classification.py
@@ -2,6 +2,14 @@ from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 
 
 def categorical_encode(values):  # *
+    """Converts categories of any type to one-hot encoded numpy array.
+
+    :param values: Collection of categorical data.
+    :type values: iterable
+    :return: One-hot encoded numpy array.
+    :rtype: numpy.ndarray
+    """
+    
     # Integer encode
     label_encoder = LabelEncoder()
     integer_encoded = label_encoder.fit_transform(values)
